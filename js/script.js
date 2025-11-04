ac8975627f1b31603785f619d8c53cbbb240dd5b
Muhammad Irfan Harits <sanubariharits@gmail.com>
2025-11-05 05:43:19 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Efek animasi lembut saat scroll
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+document.querySelectorAll('.fade-up').forEach(el => observer.observe(el));
+document.querySelectorAll('.program-card').forEach(el => observer.observe(el));
